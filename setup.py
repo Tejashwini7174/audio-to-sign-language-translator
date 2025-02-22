62049a84a9650e35667baca8b88e81cd185fe5c3
Tejashwini7174 <119780836+Tejashwini7174@users.noreply.github.com>
2025-02-22 22:37:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setuptools.setup(
     name='audio-speech-to-sign-language-converter',
     version='0.1.0',
     description='Python project',
-    author='Jigar Gajjar',
-    author_email='jigargajjar.55jg@gmail.com',
-    url='https://github.com/jigargajjar55/Audio-Speech-To-Sign-Language-Converter',
+    author='Tejashwini Poshamoni',
+    author_email='tejashwiniposhamoni@gmail.com',
+    url='https://github.com/Tejashwini7174/audio-to-sign-language-translator',
     packages=setuptools.find_packages(),
     setup_requires=['nltk', 'joblib','click','regex','sqlparse','setuptools'],
 )
